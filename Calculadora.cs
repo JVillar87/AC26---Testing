68428b5be4560d10c0b9e8c64cee8596da6430fd
JENNIFER <jennifer.villar@estudiant.fjaverianas.com>
2026-02-05 22:00:34 +0100

    TESTING

---
+++ Calculadora.cs
@@ -0,0 +1,23 @@
+public class Calculadora
+{
+    public static int Sumar(int a, int b)
+    {
+        
+        return a+b;
+    }
+
+    public int Restar(int a, int b)
+    {
+        return a-b;
+    }
+
+    public int Multiplicar(int a, int b)
+    {
+        return a*b;
+    }
+
+    public int Dividir(int a, int b)
+    {
+        return a/b;
+    }
+}
---
+++ CalculadoraTest.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+public class CalculadoraTest
+{
+    Calculadora calculadora = new Calculadora();
+
+[Fact]
+    public void ProbandoSumar()
+    {
+        int resultado = Calculadora.Sumar(5, 3);
+        Assert.Equal(8, resultado);
+        Assert.True(resultado > 0);
+        Assert.NotEqual(10, resultado);           
+    }
+
+[Fact]
+    public void ProbandoRestar()
+    {
+        int resultado = calculadora.Restar(8, 3);
+        Assert.Equal(5, resultado);
+        Assert.True(resultado > 0);
+        Assert.NotEqual(10, resultado);            
+    }
+
+[Fact]
+    public void ProbandoMultiplicar()
+    {
+        int resultado = calculadora.Multiplicar(15, 6);
+        Assert.Equal(90, resultado);
+        Assert.True(resultado > 0);
+        Assert.NotEqual(10, resultado);            
+    }
+
+[Fact]
+    public void ProbandoDividir()
+    {
+        int resultado = calculadora.Dividir(48, 8);
+        Assert.Equal(6, resultado);
+        Assert.True(resultado > 0);
+        Assert.NotEqual(10, resultado);            
+    }
+
+}
