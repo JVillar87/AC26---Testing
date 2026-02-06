7e2700ff79db4a20241676d735ad22b97d764018
JENNIFER <jennifer.villar@estudiant.fjaverianas.com>
2026-02-06 09:06:17 +0100

    add image

--- obj/Debug/net9.0/Testing.AssemblyInfo.cs
+++ obj/Debug/net9.0/Testing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68428b5be4560d10c0b9e8c64cee8596da6430fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
